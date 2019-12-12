2a126b7ad7983441f38270cc1ae94e2296b6ca46
Liu Tianqing <liutianqing@dfsoft.com>
2019-12-12 10:38:11 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// example NO.1
+
+// example NO.2
+// src/main.js
+// import { version } from '../package.json';
+
+// export default function () {
+//   console.log('version ' + version);
+// }
+
+// example NO.3
+// src/main.js
+import answer from 'the-answer';
+
+export default function () {
+  console.log('the answer is ' + answer);
+}
